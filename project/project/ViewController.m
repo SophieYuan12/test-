590ea9c6423d767286ae95e852e64c36492cae11
袁飞霞 <997462996@qq.com>
2016-07-05 22:11:53 +0800

    初次提交

---
+++ project/project/ViewController.m
@@ -0,0 +1,44 @@
+//
+//  ViewController.m
+//  project
+//
+//  Created by 袁飞霞 on 16/6/27.
+//  Copyright © 2016年 cn.sophietobeabetterone. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+//    UIView *view = [[UIView alloc]init];
+    
+}
+
+<<<<<<< HEAD
+-(void)pro1
+{
+    NSLog(@"%@", @"第一次修改");
+}
+-(void)pro2
+{
+    NSLog(@"%@", @"第二次修改");
+}
+
+<<<<<<< HEAD
+=======
+>>>>>>> parent of 663ce31... 第一次修改
+=======
+>>>>>>> parent of 9432a8b... 第三次修改
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
