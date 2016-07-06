fa11436d7fd98993fdd6ef29503c1edefb64647d
袁飞霞 <997462996@qq.com>
2016-07-06 19:38:14 +0800

    cvbn

--- project/project/ViewController.m
+++ project/project/ViewController.m
@@ -37,11 +37,16 @@
     NSLog(@"%@", @"第一次reset");
 }
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+>>>>>>> parent of bc816b7... Revert "xxx"
 
 -(void)pro5
 {
     NSLog(@"%@", @"第五次reset");
 }
+<<<<<<< HEAD
 =======
 =======
 -(void)pro3
@@ -56,6 +61,24 @@
 
 >>>>>>> 5d1227bc0db13638222fc882ccdee3abc181e1d1
 >>>>>>> e59a18df53ba4d27a1427e283a868209514778f7
+=======
+=======
+=======
+-(void)pro3
+{
+    NSLog(@"%@", @"第三次修改");
+}
+
+-(void)pro4
+{
+    NSLog(@"%@", @"第四次修改");
+}
+
+>>>>>>> 5d1227bc0db13638222fc882ccdee3abc181e1d1
+>>>>>>> e59a18df53ba4d27a1427e283a868209514778f7
+=======
+>>>>>>> parent of 0731312... 回滚后重新提交
+>>>>>>> parent of bc816b7... Revert "xxx"
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
